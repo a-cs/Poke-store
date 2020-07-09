d7d974395916bd2a6b521b004190f58bfd1e6203
a-cs <acarneirosousa@gmail.com>
2020-07-09 00:24:54 -0300

    build

---
+++ build/precache-manifest.eb3e102e93de6c607fd8cb7f1cd9b034.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e0c41520623dd5a97fb84533c71fed8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e92af952c8dd218d1ef0",
+    "url": "/static/css/main.39b21f15.chunk.css"
+  },
+  {
+    "revision": "4d8a615c6670feb61053",
+    "url": "/static/js/2.29119adc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.29119adc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e92af952c8dd218d1ef0",
+    "url": "/static/js/main.fbb41919.chunk.js"
+  },
+  {
+    "revision": "f6198c05f67f54ef751b",
+    "url": "/static/js/runtime-main.cc44eb44.js"
+  }
+]);
